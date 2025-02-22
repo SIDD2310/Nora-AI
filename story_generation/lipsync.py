83ba8980b86f3141a063672f67479b155f2c123e
Caesar <pepijndekeijzer@gmail.com>
2025-02-22 14:23:49 +1100

    organization

---
+++ story_generation/lipsync.py
@@ -0,0 +1,25 @@
+import fal_client
+
+
+
+
+def on_queue_update(update):
+    if isinstance(update, fal_client.InProgress):
+        for log in update.logs:
+           print(log["message"])
+
+
+def lipsync(audio_filename, video_filename):
+    result = fal_client.subscribe(
+        "fal-ai/sync-lipsync",
+        arguments={
+            "video_url": video_filename,
+            "audio_url": audio_filename,
+        },
+        with_logs=True,
+        on_queue_update=on_queue_update,
+    )
+    return result
+
+
+
